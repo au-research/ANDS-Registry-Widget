d2eecd8ca33e5c455155a60044ef835c9e333223
Minh Duc Nguyen <dekarvn@gmail.com>
2016-05-08 13:37:21 +1000

    Fix grunt karma not having Mustache as a prerequisite script

--- test/setup.js
+++ test/setup.js
@@ -10,8 +10,15 @@
     var $apiKey = "ANDS-Registry-Widget-QUnit-Test";
 
     // define test parameters
-    var $testPurl = 'http://purl.org/au-research/grants/arc/DP140100435'
+    var $testPurl = 'http://purl.org/au-research/grants/arc/DP140100435';
 
     $( "body" ).prepend( $testCanvas );
+
+    // include Mustache template engine
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "https://cdnjs.cloudflare.com/ajax/libs/mustache.js/2.2.1/mustache.js";
+    $("head").append(s);
+
 
 }( jQuery ) );
